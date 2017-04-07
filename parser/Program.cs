2f9067526c85b6133e1b2ab7e55eb4e8854b34b0
Andriy Khrystyanovich <andriy.khrystyanovic@wonga.com>
2017-04-07 19:13:12 +0300

    CS-02 added c# stats reader

---
+++ parser/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filename = "csstats.dat";
+            try
+            {
+                var r = StatsFile.ReadEntriesToList(filename);
+
+                foreach (var rr in r)
+                {
+                    Console.WriteLine(rr);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

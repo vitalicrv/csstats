73228db1d77cb82de7fad21c8b226ffd7c590fb3
Andriy Khrystyanovich <a.khrystyanovich@gmail.com>
2017-04-23 19:13:43 +0300

    added net core ui

---
+++ src/cs.shell/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StatsParser;
+
+namespace cs.shell.Controllers
+{
+    public class HomeController : Controller
+    {
+        const string Filename = "D:\\projects\\csstats\\data\\csstats.dat";
+
+        public IActionResult Index()
+        {
+            var entries = StatsFile.ReadEntries(Filename);
+            return View(entries);
+        }
+    }
+}
